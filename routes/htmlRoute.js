e819050e78cacfcf829afce0afb869c8c03b2d54
jwade4207 <jwade4207@yahoo.com>
2021-07-09 21:59:18 -0500

    save routes

---
+++ routes/htmlRoute.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const router = require('express').Router();
+
+//GET route to return index.html
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+//GET route to return notes.html
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+//wildcard route
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+
+module.exports = router;
